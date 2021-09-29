e28ff03ca01acf7f737d407470e95b6d3ba2ebe5
sanam1060 <sanam_g1990@yahoo.com>
2021-09-29 16:51:18 -0700

    nodemon

--- express_server.js
+++ express_server.js
@@ -4,10 +4,24 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+const generateRandomString = function(length) {
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -22,14 +36,32 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"};
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]/* What goes here? */ };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  let shortURLt = generateRandomString(6);
+  urlDatabase[shortURLt] = req.body.longURL;
+  // console.log(urlDatabase);
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const templateVars = { shortURL: shortURLt, longURL: req.body.longURL};
+
+  res.render("urls_show", templateVars);
+});
+
