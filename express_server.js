6a9a3985aee6605cdc5276095513df16a3e6397d
sanam1060 <sanam_g1990@yahoo.com>
2021-09-28 22:27:17 -0700

    Installing and Setting Up EJS

--- express_server.js
+++ express_server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+
+const express = require("express");
+const app = express();
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
@@ -15,9 +21,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
