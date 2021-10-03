9e32e424c7ec8cb2d4e23516a0f13a2154cb7e39
sanam1060 <sanam_g1990@yahoo.com>
2021-10-03 14:48:30 -0700

    Refactoring the Login Route

--- express_server.js
+++ express_server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const { redirect } = require("statuses");
 
 app.set("view engine", "ejs");
 
@@ -37,14 +50,26 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  console.log("Username", req.cookies["username"]);
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  console.log("user_id", req.cookies["user_id"]);
+  let emailtemp = {};
+  if (users[req.cookies["user_id"]]) {
+    emailtemp = users[req.cookies["user_id"]].email;
+  }
+  const templateVars = {
+    urls: urlDatabase,
+    email: emailtemp,
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.cookie("username", req.cookies["username"]);
-  res.render("urls_new", { username: req.cookies["username"] });
+  res.cookie("user_id", req.cookies["user_id"]);
+  let user = users[req.cookies["user_id"]];
+  let emailtemp = "";
+  if (users[req.cookies["user_id"]]) {
+    emailtemp = users[req.cookies["user_id"]].email;
+  }
+  res.render("urls_new", { email: emailtemp });
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -60,6 +85,21 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/register", (req, res) => {
+  res.render("urls_reg");
+});
+
+app.get("/login", (req, res) => {
+  res.render("urls_login");
+});
+
+app.get("/logout", (req, res) => {
+  // console.log("logout");
+  // res.clearCookie("username");
+  res.clearCookie("user_id");
+  return res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -71,7 +111,7 @@ app.post("/urls", (req, res) => {
   // console.log(urlDatabase);
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
   const templateVars = { shortURL: shortURLt, longURL: req.body.longURL };
-  res.cookie("username", req.body.username);
+  res.cookie("user_id", req.body.user_id);
 
   res.render("urls_show", templateVars);
 });
@@ -83,13 +123,59 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const typedName = req.body.username;
-  res.cookie("username", typedName);
+  // const typedName = req.body.username;
+  const emailUsr = req.body.email;
+  const passUsr = req.body.password;
+  console.log(emailUsr, "userEmail");
+  if (checkEmail(emailUsr)) {
+    for (let key of Object.keys(users)) {
+      // console.log(users[key]);
+      if (users[key].email === emailUsr) {
+        if (users[key].password === passUsr) {
+          res.cookie("user_id", users[key].id);
+        } else {
+          return res.status(403).end();
+        }
+      }
+    }
+  } else {
+    return res.status(403).end();
+  }
+  // res.cookie("username", typedName);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
   // console.log("logout");
-  res.clearCookie("username");
+  // res.clearCookie("username");
+  res.clearCookie("user_id");
   return res.redirect("/urls");
 });
+
+app.post("/register", (req, res) => {
+  const userEmail = req.body.email;
+  const password = req.body.password;
+  if (!userEmail || !password) {
+    return res.status(400).end();
+  }
+  if (checkEmail(userEmail)) {
+    return res.status(400).end();
+  }
+  let ID = generateRandomString(6);
+  // console.log(email);
+
+  users[ID] = { id: ID, email: userEmail, password: password };
+  res.cookie("user_id", ID);
+  return res.redirect("/urls");
+});
+
+const checkEmail = function (emailTemp) {
+  for (let key of Object.keys(users)) {
+    console.log(users[key]);
+    if (users[key].email === emailTemp) {
+      return true;
+    }
+  }
+
+  return false;
+};
